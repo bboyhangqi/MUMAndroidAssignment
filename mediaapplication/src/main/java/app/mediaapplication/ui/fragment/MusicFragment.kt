6fe6ca9a75a7fa8c4b241c2a0a17aaef2ad7b0c0
Nhan Nguyen <nhannguyenit.work@gmail.com>
2018-04-16 09:42:40 -0500

    no message

--- mediaapplication/src/main/java/app/mediaapplication/ui/fragment/MusicFragment.kt
+++ mediaapplication/src/main/java/app/mediaapplication/ui/fragment/MusicFragment.kt
@@ -1,13 +1,16 @@
 package app.mediaapplication.ui.fragment
 
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 import app.mediaapplication.R
+import kotlinx.android.synthetic.main.fragment_music.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,12 +22,51 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class MusicFragment : Fragment() {
+    private lateinit var mp : MediaPlayer
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false)
+
+        val view: View = inflater!!.inflate(R.layout.fragment_music, container, false)
+
+        mp = MediaPlayer.create (getActivity(), R.raw.music)
+        var position = 0
+
+
+        val playBTN = view.findViewById<Button>(R.id.playBTN)
+        playBTN.setOnClickListener {
+            mp.start()
+        }
+
+        val pauseBTN = view.findViewById<Button>(R.id.pauseBTN)
+        pauseBTN.setOnClickListener {
+            if (mp.isPlaying ()) {
+                position = mp.getCurrentPosition ()
+                mp.pause ()
+            }
+        }
+
+        val contBTN = view.findViewById<Button>(R.id.contBNT)
+        contBTN.setOnClickListener {
+            if (mp.isPlaying () == false) {
+                mp.seekTo (position)
+                mp.start ()
+            }
+        }
+
+        val stopBTN = view.findViewById<Button>(R.id.stopBTN)
+        stopBTN.setOnClickListener {
+            mp.pause ()
+            position = 0
+            mp.seekTo (0)
+        }
+
+        return view
     }
 
-
+    override fun onDestroy () {
+        super.onDestroy ()
+        mp.release ()
+    }
 }
--- mediaapplication/src/main/java/app/mediaapplication/ui/fragment/VideoFragment.kt
+++ mediaapplication/src/main/java/app/mediaapplication/ui/fragment/VideoFragment.kt
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
 
 import app.mediaapplication.R
+import kotlinx.android.synthetic.main.fragment_video.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,11 +22,29 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class VideoFragment : Fragment() {
+    private var mediaController: MediaController? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_video, container, false)
+
+        val videoView = view.findViewById<VideoView>(R.id.videoView)
+
+        configureVideoView(videoView)
+
+        return view
+    }
+
+    private fun configureVideoView(videoView: VideoView) {
+
+        videoView.setVideoPath(
+                "http://techslides.com/demos/sample-videos/small.mp4")
+
+        mediaController = MediaController(getActivity())
+        mediaController?.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
     }
 
 
